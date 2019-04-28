95ede3f499ddea81c488c4c5cf1b98b05a792bfa
lyuouz <emiliac421@gmail.com>
2019-04-28 14:19:29 -0400

    update functions to include usernames

--- app.R
+++ app.R
@@ -1,36 +1,8 @@
-library(shiny)
-
-# Define UI ----
-ui <- fluidPage(
-  
-  titlePanel("AirCasting Data Analysis"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      helpText('Customize your analysis using the AirBeam data'),
-      dateRangeInput("dates", h4("Choose start and end date"))
-      
-      ),
-  
-    
-    mainPanel()
-  )
-      
-  
-)
-
-# Define server logic ----
-server <- function(input, output) {
-  
-  
-  
-  
-  
-  
-  
-  
-  
-}
-
-# Run the app ----
-shinyApp(ui = ui, server = server)
+---
+title: "AirCasting Dashboard"
+output: 
+  flexdashboard::flex_dashboard:
+  theme: journal
+social: menu
+source_code: embed
+---
